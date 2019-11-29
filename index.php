3e020152526c6b61adc67b4c5c6fda3129b39b99
Aluno <Aluno@sn613>
2019-11-28 23:26:34 -0200

    Finalizado

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+  <script src="js/clima.js"></script>
+  
+    </head>
+    <body>
+        
+        <div class="row">
+            
+            <div class="col-md-12">
+               <div class="p-4 mb-2 bg-danger text-white"></div>
+               <div class="jumbotron">
+                <h4>Temperatura SC
+                    <span id="temperatura"></span></h1>
+                <p>
+              
+                    <span id="situacao"></span>
+                    <img id="icone" src="img/icones/02d.png"/>
+                </p>
+                
+                <h4>
+
+                <h4>
+                    Pressão do Ar: <span id="pressaoAr">10</span>
+                    <p>
+                    <img src="img/icones/02n.png"/>
+                    </p>
+                </h4>
+                
+                <h4>
+                    Umidade: <span id="umidade">100%</span>
+                    <p>
+                    <img src="img/icones/01d.png"/>
+                    </p>
+                </h4>
+                
+                
+                <h4>
+                    Temperatura Mínima: <span id="tempminima">294.82</span>
+                    <p>
+                    <img src="img/icones/04n.png"/>
+                    </p>
+                </h4>
+                <h4>
+                   Temperatura Máxima: <span id="tempmaxima">296.48</span>
+                   <p>
+                   <img src="img/icones/09d.png"/>
+                   </p>
+                </h4>
+                
+                <h4>
+                    Velocidade do vento: <span id="vento">1.5</span>
+                    <p>
+                    <img src="img/icones/01n.png"/>
+                    </p>
+                </h4>
+                    <h4>
+                    Amanhecer: <span id="amanhecer"></span>
+                    
+                </h4>
+                    
+                     <h4>
+                    Por do Sol: <span id="pordosol"></span>
+                    
+                </h4>
+            </div>
+            </div>
+        </div>
+        
+        
+       
+            
+            
+    </body>
+</html>

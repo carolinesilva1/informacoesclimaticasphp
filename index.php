9e49b56bf9d3d6593dfbf17071c6b6584fe2e414
Aluno <Aluno@sn613>
2019-11-30 02:32:24 -0200

    Finalizado

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ and open the template in the editor.
 <html>
     <head>
         
-        <div class="p-3 mb-2 bg-danger text-white">.bg-danger</div>
+        
 
         <title>Clima</title>
         <meta charset="UTF-8">
